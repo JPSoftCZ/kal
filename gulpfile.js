e89fdc022a7a4776af1cff13d43317ee6811a83d
Josef Poula <pepcapoula@seznam.cz>
2022-01-10 19:24:34 +0100

    efe

--- gulpfile.js
+++ gulpfile.js
@@ -1,199 +1,75 @@
-'use strict';
+var gulp = require('gulp');
+var less = require('gulp-less');
+var babel = require('gulp-babel');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var cleanCSS = require('gulp-clean-css');
+var del = require('del');
 
-const sass = require('gulp-sass')(require('sass'));
-const gulp = require('gulp');
-const sourcemaps = require('gulp-sourcemaps');
-const fileinclude = require('gulp-file-include');
-const autoprefixer = require('gulp-autoprefixer');
-const bs = require('browser-sync').create();
-const rimraf = require('rimraf');
-const comments = require('gulp-header-comment');
-
-var path = {
-  src: {
-    html: 'source/*.html',
-    others: 'source/*.+(php|ico|png)',
-    htminc: 'source/partials/**/*.htm',
-    incdir: 'source/partials/',
-    plugins: 'source/plugins/**/*.*',
-    js: 'source/js/*.js',
-    scss: 'source/scss/**/*.scss',
-    images: 'source/images/**/*.+(png|jpg|gif|svg)'
+var paths = {
+  styles: {
+    src: 'src/styles/**/*.less',
+    dest: 'assets/styles/'
   },
-  build: {
-    dirNetlify: 'netlify/',
-    dirDev: 'theme/'
+  scripts: {
+    src: 'src/scripts/**/*.js',
+    dest: 'assets/scripts/'
   }
 };
 
-// HTML
-gulp.task('html:build', function () {
-  return gulp.src(path.src.html)
-    .pipe(fileinclude({
-      basepath: path.src.incdir
+/* Not all tasks need to use streams, a gulpfile is just another node program
+ * and you can use all packages available on npm, but it must return either a
+ * Promise, a Stream or take a callback and call it
+ */
+function clean() {
+  // You can use multiple globbing patterns as you would with `gulp.src`,
+  // for example if you are using del 2.0 or above, return its promise
+  return del([ 'assets' ]);
+}
+
+/*
+ * Define our tasks using plain functions
+ */
+function styles() {
+  return gulp.src(paths.styles.src)
+    .pipe(less())
+    .pipe(cleanCSS())
+    // pass in options to the stream
+    .pipe(rename({
+      basename: 'main',
+      suffix: '.min'
     }))
-    .pipe(comments(`
-    WEBSITE: https://themefisher.com
-    TWITTER: https://twitter.com/themefisher
-    FACEBOOK: https://www.facebook.com/themefisher
-    GITHUB: https://github.com/themefisher/
-    `))
-    .pipe(gulp.dest(path.build.dirDev))
-    .pipe(bs.reload({
-      stream: true
-    }));
-});
+    .pipe(gulp.dest(paths.styles.dest));
+}
 
-// SCSS
-gulp.task('scss:build', function () {
-  return gulp.src(path.src.scss)
-    .pipe(sourcemaps.init())
-    .pipe(sass({
-      outputStyle: 'expanded'
-    }).on('error', sass.logError))
-    .pipe(autoprefixer())
-    .pipe(sourcemaps.write('/'))
-    .pipe(comments(`
-    WEBSITE: https://themefisher.com
-    TWITTER: https://twitter.com/themefisher
-    FACEBOOK: https://www.facebook.com/themefisher
-    GITHUB: https://github.com/themefisher/
-    `))
-    .pipe(gulp.dest(path.build.dirDev + 'css/'))
-    .pipe(bs.reload({
-      stream: true
-    }));
-});
+function scripts() {
+  return gulp.src(paths.scripts.src, { sourcemaps: true })
+    .pipe(babel())
+    .pipe(uglify())
+    .pipe(concat('main.min.js'))
+    .pipe(gulp.dest(paths.scripts.dest));
+}
 
-// Javascript
-gulp.task('js:build', function () {
-  return gulp.src(path.src.js)
-    .pipe(comments(`
-  WEBSITE: https://themefisher.com
-  TWITTER: https://twitter.com/themefisher
-  FACEBOOK: https://www.facebook.com/themefisher
-  GITHUB: https://github.com/themefisher/
-  `))
-    .pipe(gulp.dest(path.build.dirDev + 'js/'))
-    .pipe(bs.reload({
-      stream: true
-    }));
-});
+function watch() {
+  gulp.watch(paths.scripts.src, scripts);
+  gulp.watch(paths.styles.src, styles);
+}
 
-// Images
-gulp.task('images:build', function () {
-  return gulp.src(path.src.images)
-    .pipe(gulp.dest(path.build.dirDev + 'images/'))
-    .pipe(bs.reload({
-      stream: true
-    }));
-});
+/*
+ * Specify if tasks run in series or parallel using `gulp.series` and `gulp.parallel`
+ */
+var build = gulp.series(clean, gulp.parallel(styles, scripts));
 
-// Plugins
-gulp.task('plugins:build', function () {
-  return gulp.src(path.src.plugins)
-    .pipe(gulp.dest(path.build.dirDev + 'plugins/'))
-    .pipe(bs.reload({
-      stream: true
-    }));
-});
-
-// Other files like favicon, php, sourcele-icon on root directory
-gulp.task('others:build', function () {
-  return gulp.src(path.src.others)
-    .pipe(gulp.dest(path.build.dirDev))
-});
-
-// Clean Build Folder
-gulp.task('clean', function (cb) {
-  rimraf('./theme', cb);
-});
-
-// Watch Task
-gulp.task('watch:build', function () {
-  gulp.watch(path.src.html, gulp.series('html:build'));
-  gulp.watch(path.src.htminc, gulp.series('html:build'));
-  gulp.watch(path.src.scss, gulp.series('scss:build'));
-  gulp.watch(path.src.js, gulp.series('js:build'));
-  gulp.watch(path.src.images, gulp.series('images:build'));
-  gulp.watch(path.src.plugins, gulp.series('plugins:build'));
-});
-
-// Build Task
-gulp.task('default', gulp.series(
-  'clean',
-  'html:build',
-  'js:build',
-  'scss:build',
-  'images:build',
-  'plugins:build',
-  'others:build',
-  gulp.parallel(
-    'watch:build',
-    function () {
-      bs.init({
-        server: {
-          baseDir: path.build.dirDev,
-        }
-      });
-    })
-  )
-);
-
-
-/* =====================================================
-Netlify Builds
-===================================================== */
-// HTML
-gulp.task('html:netlify:build', function () {
-  return gulp.src(path.src.html)
-    .pipe(fileinclude({
-      basepath: path.src.incdir
-    }))
-    .pipe(gulp.dest(path.build.dirNetlify));
-});
-
-// SCSS
-gulp.task('scss:netlify:build', function () {
-  return gulp.src(path.src.scss)
-    .pipe(sourcemaps.init())
-    .pipe(sass({
-      outputStyle: 'expanded'
-    }).on('error', sass.logError))
-    .pipe(autoprefixer())
-    .pipe(sourcemaps.write('/maps'))
-    .pipe(gulp.dest(path.build.dirNetlify + 'css/'));
-});
-
-// Javascript
-gulp.task('js:netlify:build', function () {
-  return gulp.src(path.src.js)
-    .pipe(gulp.dest(path.build.dirNetlify + 'js/'));
-});
-
-// Images
-gulp.task('images:netlify:build', function () {
-  return gulp.src(path.src.images)
-    .pipe(gulp.dest(path.build.dirNetlify + 'images/'));
-});
-
-// Plugins
-gulp.task('plugins:netlify:build', function () {
-  return gulp.src(path.src.plugins)
-    .pipe(gulp.dest(path.build.dirNetlify + 'plugins/'))
-});
-
-// Other files like favicon, php, apple-icon on root directory
-gulp.task('others:netlify:build', function () {
-  return gulp.src(path.src.others)
-    .pipe(gulp.dest(path.build.dirNetlify))
-});
-
-// Build Task
-gulp.task('netlify', gulp.series(
-  'html:netlify:build',
-  'js:netlify:build',
-  'scss:netlify:build',
-  'images:netlify:build',
-  'plugins:netlify:build'
-));
+/*
+ * You can use CommonJS `exports` module notation to declare tasks
+ */
+exports.clean = clean;
+exports.styles = styles;
+exports.scripts = scripts;
+exports.watch = watch;
+exports.build = build;
+/*
+ * Define default task that can be called by just running `gulp` from cli
+ */
+exports.default = build;
